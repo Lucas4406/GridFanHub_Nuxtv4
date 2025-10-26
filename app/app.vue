76e730f01a53db4b548f24ca43731e2b38587436
Lucas <lucas.popescu41@gmail.com>
2025-10-26 17:33:28 +0200

    bang

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-black font-bold">Hello world!</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+    '@formkit/auto-animate',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxthub/core'
+  ]
 })
