e47d1e94c2de0d9c7ce6549677fe5ad40dc69cb9
Lucas <lucas.popescu41@gmail.com>
2025-11-21 12:03:46 +0200

    news + updated navbar

--- app/stores/newsStore.ts
+++ app/stores/newsStore.ts
@@ -1,10 +1,10 @@
-const runtimeConfig = useRuntimeConfig()
 export const useNewsStore = defineStore('newsStore', {
     state: () => ({
         newsArray: []
     }),
     actions: {
         async fetch () {
+            const runtimeConfig = useRuntimeConfig()
             const news = await $fetch<any>(`${runtimeConfig.public.cloudflareProxyBase}/v1/editorial/articles?limit=9`)
 
             this.newsArray = news.items
--- app/stores/videosStore.ts
+++ app/stores/videosStore.ts
@@ -1,10 +1,10 @@
-const runtimeConfig = useRuntimeConfig()
 export const useVideosStore = defineStore('videosStore', {
     state: () => ({
         videosArray: []
     }),
     actions: {
         async fetch () {
+            const runtimeConfig = useRuntimeConfig()
             const videosCall = await $fetch<any>(`${runtimeConfig.public.cloudflareProxyBase}/v1/video-assets/videos?limit=9`)
 
             this.videosArray = videosCall.videos
