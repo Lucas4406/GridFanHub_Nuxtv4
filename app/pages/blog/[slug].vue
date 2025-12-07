a704f8d0d0a43cdda11a5b637a308483221bbe0d
Lucas <lucas.popescu41@gmail.com>
2025-12-07 18:15:18 +0200

    update next race card

--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -5,8 +5,70 @@ const { data: post } = await useAsyncData(`blog-${slug}`, () => {
   return queryCollection('content').path(`/blog/${slug}`).first()
 })
 
+
+// 🔹 Dynamic SEO / Meta
+useHead(() => {
+  if (!post.value) return {}
+
+  return {
+    title: post.value.title,
+    meta: [
+      { name: 'description', content: post.value.description },
+      { name: 'keywords', content: post.value.tags.join(', ') },
+      { property: 'og:title', content: post.value.title },
+      { property: 'og:description', content: post.value.description },
+      { property: 'og:image', content: post.value.image },
+      { property: 'og:type', content: 'article' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: post.value.title },
+      { name: 'twitter:description', content: post.value.description },
+      { name: 'twitter:image', content: post.value.image }
+    ],
+    link: [
+      { rel: 'canonical', href: `https://beta.gridfanhub.com/blog/${slug}` }
+    ]
+  }
+})
+
+
 </script>
 
 <template>
-    <ContentRenderer v-if="post" :value="post" />
+  <UPage>
+    <UPageBody>
+      <UContainer>
+        <NuxtLink
+            to="/blog"
+            class="text-black dark:text-white hover:underline font-medium inline-block"
+        >
+          ← Back
+        </NuxtLink>
+        <UPageHeader :title="post.title" :headline="new Date(post.date).toLocaleDateString()" >
+          <template #description>
+            <div class="flex flex-row gap-2">
+              <div v-for="(postTag, index) in post.tags.slice(0,2)" :key="index" class="flex flex-row items-center gap-2">
+                <UBadge :label="postTag" color="primary" size="sm" />
+              </div>
+            </div>
+          </template>
+        </UPageHeader>
+        <div class="w-full flex justify-center py-4" v-if="post.image?.includes('media.formula1.com')" >
+          <NuxtImg :src="post.image" :alt="post.location" class="w-full" />
+        </div>
+        <UContainer>
+          <ContentRenderer v-if="post" :value="post" />
+          <UUser
+              orientation="horizontal"
+              :name="post.author"
+              description="Admin"
+              size="md"
+              :avatar="{
+                  src: '/favicon.png'
+                }"
+              class="flex justify-center"
+          />
+        </UContainer>
+      </UContainer>
+    </UPageBody>
+  </UPage>
 </template>
--- content/blog/abu-dhabi-grand-prix-2025-in-100-words.md
+++ content/blog/abu-dhabi-grand-prix-2025-in-100-words.md
@@ -10,4 +10,18 @@ author: "GridFanHub Team"
 image: "https://media.formula1.com/image/upload/content/dam/fom-website/2018-redesign-assets/Racehub%20header%20images%2016x9/Abu%20Dhabi.jpg"
 ---
 
-The 2025 Abu Dhabi Grand Prix delivered a dramatic finale under the Yas Marina lights. Strategic gambles, late-race pace swings, and intense midfield battles defined the season’s closing chapter. Overtakes into Turn 9 showcased the circuit’s improvements, while varying tyre strategies kept the outcome unpredictable until the final laps. The leaders pushed to the limit, managing degradation and traffic in equal measure. Fans witnessed bold defensive driving, impressive comebacks, and one last showdown between top contenders. As the chequered flag fell, the race encapsulated the tension and excitement that shaped the entire 2025 Formula 1 season.
+The **2025 Abu Dhabi Grand Prix** delivered a dramatic finale under the Yas Marina lights. Strategic gambles, late-race pace swings, and intense midfield battles defined the season’s closing chapter. 
+
+The three key competitors – Max, Norris, and Piastri all battled it out to the chequered flag – but Max Verstappen came on top. Unfortunately, he was just shy of Lando in the Drivers’ Championship crowning Lando Norris the **F1 Champion of the world** in 2025. 
+
+There were also very close battles in the midfield – most notably Lewis Hamilton coming through from P16 to finish P8. May the celebration begin for the Champion!
+
+## Results
+| Position | Driver         | Team            | Time/Retired |
+|----------|----------------|-----------------|--------------|
+| 1        | Max Verstappen | Red Bull Racing | 1:26:07.469  |
+| 2        | Oscar Piastri  | McLaren         | 	+12.594s    |
+| 3        | Lando Norris   | McLaren         | 	+16.572s    |
+
+### Note
+This marks my first post of this type, and I’m excited to share even more next season.
