cea4cff2bd5cf940e41a6b3b1a24bbc5c3858747
Lucas <lucas.popescu41@gmail.com>
2026-02-13 02:11:12 +0200

    updatat pt 2026

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -50,7 +50,7 @@ const providers = [
 
 const schema = z.object({
   email: z.email('Invalid email'),
-  password: z.string('Password is required').min(8, 'Must be at least 8 characters')
+  password: z.string('Password is required').min(8, 'Must be at least 8 characters long.')
 })
 
 type Schema = z.output<typeof schema>
